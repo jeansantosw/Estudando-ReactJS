c5a0f95a685661d10b6033c534352c46b9ebd9d9
Jean Santos <jeansantos.sw@gmail.com>
2021-03-06 14:01:43 +0000

    Atualziar CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 import Header from './components/Header';
 
 function App() {
+    const [ projects, setProjects ] = useState(['Reactjs', 'NodeJS']);
+
+    function handleAddProject(){
+
+        //projects.push(`React Native ${Date.now()}`);
+        setProjects([...projects, `React Native ${Date.now()}`]);
+    }
+
     return(
         <>
-        <Header  title="HomePage">
+        <Header  title="Projects"/>
+        <ul>
+            {projects.map(project=> <li key={project}>{project}</li>)}
+        </ul>
 
-            <ul>
-                <li>ReactJS</li>
-                <li>NodeJS</li>
-            </ul>
-            
-        </Header>,
-
-        <Header  title="Projects">
-
-            <ul>
-                <li>React Native</li>
-            </ul>
-        </Header>
+        <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
         </>
     );
 }
